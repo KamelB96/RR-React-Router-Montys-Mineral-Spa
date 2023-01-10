592d449e5149a428ced176a92916d69463721fb4
kamel Barakat <kamelbarakat96@yahoo.com>
2023-01-09 20:58:25 -0500

    bootstrap upgrade

--- react-router/src/components/Home.js
+++ react-router/src/components/Home.js
@@ -1,13 +1,14 @@
-export default function Home () {
-    return(
-        <div className="homepage">
-            <h1>Visit our resort and change your life!</h1>
-
-            <h1>Grand opening in StressVille, Texas</h1>
-            
-            <h1>Check out our wide array of packages and offers!</h1>
-
+export default function About () {
+    return (
+        <div className="aboutpage">
+            <div className="aboutHeader">
+                <h1>About Us</h1>
+            </div>
+            <p>When returning from a spritual journey through the Earth, Monty McMansions decided to dedicate some of his fortune to offering spritiual wellness for others back in his hometown. </p>
+            <p>As the heir of the industrious McMansions fortune, Monty was never satisfied with the material wealth that provided him everything he desired. </p>
+            <p>Monty decided to travel around the world to find peace and happiness</p>
+            <p>Upon his return, he dedicated about 1% of his wealth to open this Mineral SPA to bring joy and spiritual happiness to others</p>
+            <img src="hotsprings.jpg" alt="hotsprings" />I 
         </div>
     )
-
 }
